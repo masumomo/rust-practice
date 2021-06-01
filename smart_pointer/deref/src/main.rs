e648d3876c5a1b0991a9924747e95ac8e87b93ae
Miki Masumoto <masumotomiki18@gmail.com>
2021-06-02 08:51:17 +0900

    Working on smart pointer deref

---
+++ smart_pointer/deref/src/main.rs
@@ -0,0 +1,29 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = &x;
+    let y2 = Box::new(x);
+    let y3 = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *y2);
+    assert_eq!(5, *y3);
+}
