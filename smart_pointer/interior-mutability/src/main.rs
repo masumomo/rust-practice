41ecf1b61f770016cccd851e94218af49dc3156e
Miki Masumoto <masumotomiki18@gmail.com>
2021-06-04 09:14:47 +0900

    Working on ref cycles

--- smart_pointer/interior-mutability/src/main.rs
+++ smart_pointer/interior-mutability/src/main.rs
@@ -1,49 +1,24 @@
-pub trait Messenger {
-    fn send(&self, msg: &str);
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
 }
 
-pub struct LimitTracker<'a T:Messenger> {
-    messenger: &'a T,
-    value :usize,
-    max:usize,
-}
-impl<'a, T> LimitTracker<'a T>
-where 
-T:Messenger,
-{
-    pub fn new(messenger: &T, max: usize) -> LimitTracker<T>{
-        LimitTracker{
-            messenger, 
-            value: 0,
-            max
-        }
-    }
-    pub fn set_value(&mut self. value:usize){
-        self.value = value;
-        let percentage_of_max = self.value as f64 /self.max as f64;
-
-        if percentage_of_max >= 1.0 {
-            self.messenger.send("Error: You are over your quota!");
-        }else if percentage_of_max >= 0.9 {
-            self.messenger
-                .send("Urgent warning: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
-            self.messenger
-                .send("Warning: You've used up over 75% of your quota!");
-        }
-    }
-}
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
-    // Rc<T> enables multiple owners of the same data;
-    //  Box<T> and RefCell<T> have single owners.
+    let value = Rc::new(RefCell::new(5));
 
-    // Box<T> allows immutable or mutable borrows checked at compile time;
-    //  Rc<T> allows only immutable borrows checked at compile time;
-    //  RefCell<T> allows immutable or mutable borrows checked at runtime.
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
 
-    // Because RefCell<T> allows mutable borrows checked at runtime,
-    //  you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
 
+    *value.borrow_mut() += 10;
 
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
